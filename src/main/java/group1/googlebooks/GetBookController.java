e3dbcd4e65f563ade81c7bf5c1210ecc9c2c36cc
Devlin Glasman <devlin.glasman@gmail.com>
2021-05-28 15:32:44 +0200

    Add ThymeLeaf HTML templates

--- src/main/java/group1/googlebooks/GetBookController.java
+++ src/main/java/group1/googlebooks/GetBookController.java
@@ -1,12 +1,13 @@
 package group1.googlebooks;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-@RestController
+@Controller
 public class GetBookController {
 
     private final RestTemplate restTemplate;
@@ -15,9 +16,12 @@ public class GetBookController {
         this.restTemplate = restTemplateBuilder.build();
     }
 
-    @GetMapping("/")
-    public VolumesCollection getBook(@RequestParam(value = "name", defaultValue = "World") String name) {
-        String url = "https://www.googleapis.com/books/v1/volumes?key=AIzaSyB8PrVZN2nGqgHp1BJ6M3RPIuH1ME0Vaw0&q={name}&maxResults=5";
-        return this.restTemplate.getForObject(url, VolumesCollection.class, name);
+    @GetMapping("/greeting")
+    public String getBook(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+//    public String getBook() {
+        return "greeting";
+//        String url = "https://www.googleapis.com/books/v1/volumes?key=AIzaSyB8PrVZN2nGqgHp1BJ6M3RPIuH1ME0Vaw0&q={name}&maxResults=5";
+//        return this.restTemplate.getForObject(url, VolumesCollection.class, name);
     }
 }
